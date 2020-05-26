75c2466c2b4e20207a51d2e8f47656ca9e643d65
GAnjali <GAnjali@github.com>
2020-05-26 19:33:17 +0530

    Implement a basic dropdown in react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
-import React from 'react';
+import React, {Component} from 'react';
+import DropDown from './DropDown';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Stock Visualizer</h1>
-      </header>
-    </div>
-  );
+const arrayOfData = [{
+    id: '1 - Jerry',
+    name: 'Jerry'
+},
+    {
+        id: '2 - Elaine',
+        name: 'Elaine'
+    },
+    {
+        id: '3 - Kramer',
+        name: 'Kramer'
+    },
+    {
+        id: '4 - George',
+        name: 'George'
+    },];
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedValue: 'Nothing selected',
+        }
+    }
+
+    handleSelectChange = (selectedValue) => {
+        this.setState({
+            selectedValue: selectedValue
+        });
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Stock Visualizer</h1>
+                </header>
+                <div className="App-intro">
+                    <DropDown arrayOfData={arrayOfData} onSelectChange={this.handleSelectChange}/> <br/><br/>
+                    <div>
+                        Selected value: {this.state.selectedValue}
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/DropDown.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+
+class DropDown extends Component {
+
+    handleChange = (event) => {
+        let selectedValue = event.target.value;
+        this.props.onSelectChange(selectedValue);
+    };
+
+    render() {
+        let arrayOfData = this.props.arrayOfData;
+        let options = arrayOfData.map((data) =>
+            <option
+                key={data.id}
+                value={data.id}
+            >
+                {data.name}
+            </option>
+        );
+
+        return (
+            <select name="customSearch" className="custom-search-select" onChange={this.handleChange}>
+                <option>Select Stock</option>
+                {options}
+            </select>
+        )
+    }
+}
+
+export default DropDown;
