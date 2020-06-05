c0d12cce5d3ece246037ca6ebc7c76c73a081544
GAnjali <GAnjali@github.com>
2020-06-05 21:39:20 +0530

    Refactor DropDown.js

--- src/DropDown.js
+++ src/DropDown.js
@@ -1,30 +1,18 @@
-import React, {Component} from 'react';
+import React  from 'react';
+import Select from 'react-select';
 
-class DropDown extends Component {
+const DropDown = (props) => {
+    let arrayOfData = props.arrayOfData;
+    let options = arrayOfData.map((data) => {
+            return {value: data, label: data}
+        }
+    );
 
-    handleChange = (event) => {
-        let selectedValue = event.target.value;
-        this.props.onSelectChange(selectedValue);
-    };
-
-    render() {
-        let arrayOfData = this.props.arrayOfData;
-        let options = arrayOfData.map((data) =>
-            <option
-                key={data}
-                value={data}
-            >
-                {data}
-            </option>
-        );
-
-        return (
-            <select name="customSearch" className="custom-search-select" onChange={this.handleChange}>
-                <option>Select Stock</option>
-                {options}
-            </select>
-        )
-    }
-}
+    return (
+        <Select id="stock-selection" options={options} onChange={(selectedOption) => {
+            props.onSelectChange(selectedOption.value);
+        }}/>
+    )
+};
 
 export default DropDown;
