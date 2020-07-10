1246ad98c57fa2a24a752273a4a0fe928f8fb493
GAnjali <GAnjali@github.com>
2020-07-10 20:16:27 +0530

    Make use of useState hooks and refactor DatePicker even simpler

--- src/components/CompareStocks/DatePicker.js
+++ src/components/CompareStocks/DatePicker.js
@@ -1,39 +1,28 @@
-import React, {Component} from "react";
+import React, {useState} from "react";
 import {SingleDatePicker} from "react-dates";
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
 import "../../styles/react_dates_overrides.css"
 import "../../styles/styles.css";
 
-class DatePicker extends Component {
-    state = {
-        focusedInput: false,
-    };
-
-    handleFocusChange = ({focused}) => this.setState({focusedInput: focused});
-
-    handleDateChange = (date) => {
-        this.props.handleDateChange(date);
-    };
-
-    render() {
-        return (
-            <div>
-                <h3 className={"date-picker-label"}>{"Select Date:"}</h3>
-                <div className={"single-date-picker"}>
-                    <SingleDatePicker
-                        id='date'
-                        date={this.props.date}
-                        onDateChange={this.handleDateChange}
-                        focused={this.state.focusedInput}
-                        onFocusChange={this.handleFocusChange}
-                        numberOfMonths={1}
-                        isOutsideRange={() => false}
-                    />
-                </div>
+const DatePicker = (props) => {
+    const [focused, handleFocusChange] = useState(false);
+    return (
+        <div>
+            <h3 className={"date-picker-label"}>{"Select Date:"}</h3>
+            <div className={"single-date-picker"}>
+                <SingleDatePicker
+                    id="date"
+                    date={props.date}
+                    onDateChange={(date) => props.handleDateChange(date)}
+                    focused={focused}
+                    onFocusChange={({focused}) => handleFocusChange(focused)}
+                    numberOfMonths={1}
+                    isOutsideRange={() => false}
+                />
             </div>
-        )
-    }
-}
+        </div>
+    )
+};
 
 export default DatePicker;
